4588069ac2d39f17ccc6e303265023fa561ab3a1
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-20 14:29:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,7 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |name, num|
-    # counter = 1 
-    #   until num == counter 
-    #     if num == 0 
-    #       nil 
-    #     elsif num == counter 
-    #       name 
-    #       break 
-    #     end 
-    #     counter += 1 
+
       end 
     end 
   #binding.pry 
