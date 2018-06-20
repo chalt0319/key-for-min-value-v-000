7b9322e9e93f2926dc9f8dbd589c9ffa9f82b9a5
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-20 15:05:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,8 @@ def key_for_min_value(name_hash)
       if lowest_value == 0 || value < lowest_value
         lowest_value = value 
         lowest_key = key 
+      else 
+        nil 
       end 
   end 
   lowest_key
