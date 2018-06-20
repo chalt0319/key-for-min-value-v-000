bdd6d0223138012d38dd7eb7b5412190c8f6cd0c
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-20 14:40:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
+  name_hash.sort do |key, value|
     key <=> value
     name_hash[0]
   end 
